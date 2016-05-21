8fd7d6fc295f19ae50871a1d4a2b6d05875ad811
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-21 14:42:44 -0400

    fix test

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMai.index);
+router.get('/', ctrlMain.index);
 
 module.exports = router;
