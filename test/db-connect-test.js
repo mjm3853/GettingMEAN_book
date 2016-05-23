19b95a908ccf95db4c16d5b936210756695cedd2
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-23 19:45:39 -0400

    db connection test

--- test/db-connect-test.js
+++ test/db-connect-test.js
@@ -1,3 +1,16 @@
 var expect = require("chai").expect;
 var mongoose = require("mongoose");
-var db = require("../app_server/models/db");
+
+var common = require('../common');
+var config = common.config();
+
+var dbURI = config.db;
+
+describe('DB connection tests', () => {
+
+    it('should be able to connect to the database', () => {
+        expect(mongoose.Connection.STATES.connected === mongoose.connection.readyState).to.be.true;
+    });
+
+});
+
