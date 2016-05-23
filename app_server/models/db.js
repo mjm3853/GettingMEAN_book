d0386d6cc08ca6f9d06ac9b1d2d42cea6e5a28f4
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-23 19:13:04 -0400

    probably not best practice config implementation

---
+++ app_server/models/db.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var common = require('../../common');
+var config = common.config();
+
+var dbURI = config.db;
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+   console.log('Mongoose connected to ' + dbURI); 
+});
+
+mongoose.connection.on('error', (err) => {
+   console.log('Mongoose connection error: ' + err); 
+});
+
+mongoose.connection.on('disconnected', () => {
+   console.log('Mongoose disconnected from ' + dbURI); 
+});
+
---
+++ test/db-connect-test.js
@@ -0,0 +1,3 @@
+var expect = require("chai").expect;
+var mongoose = require("mongoose");
+var db = require("../app_server/models/db");
