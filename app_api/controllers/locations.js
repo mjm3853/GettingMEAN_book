ae640bf647529af005027eb81adc783082146dc8
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-06-06 19:48:59 -0400

    error handling for api call to find a location by its id

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -7,25 +7,40 @@ var sendJsonResponse = (res, status, content) => {
 };
 
 module.exports.locationsListByDistance = (req, res) => {
-    sendJsonResponse(res, 200, {'status' : 'success'});
+    sendJsonResponse(res, 200, { 'status': 'success' });
 };
 
 module.exports.locationsCreate = (req, res) => {
-    sendJsonResponse(res, 200, {'status' : 'success'});
+    sendJsonResponse(res, 200, { 'status': 'success' });
 };
 
 module.exports.locationsReadOne = (req, res) => {
-    Loc
-        .findById(req.params.locationid)
-        .exec((err, location) => {
-            sendJsonResponse(res, 200, location);
+    if (req.params && req.params.locationid) {
+        Loc
+            .findById(req.params.locationid)
+            .exec((err, location) => {
+                if (!location) {
+                    sendJsonResponse(res, 404, {
+                        "message": "locationid not found"
+                    });
+                    return;
+                } else if (err) {
+                    sendJsonResponse(res, 404, err);
+                    return;
+                }
+                sendJsonResponse(res, 200, location);
+            });
+    } else {
+        sendJsonResponse(res, 404, {
+            "message": "No locationid in request"
         });
+    }
 };
 
 module.exports.locationsUpdateOne = (req, res) => {
-    sendJsonResponse(res, 200, {'status' : 'success'});
+    sendJsonResponse(res, 200, { 'status': 'success' });
 };
 
 module.exports.locationsDeleteOne = (req, res) => {
-    sendJsonResponse(res, 200, {'status' : 'success'});
+    sendJsonResponse(res, 200, { 'status': 'success' });
 };
