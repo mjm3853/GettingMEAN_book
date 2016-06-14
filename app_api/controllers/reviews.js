185da1d43ac1f7b6b197d34e5f5044afada6357d
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-06-13 23:06:50 -0400

    controller and functiosn to add a review. untested

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -6,8 +6,75 @@ var sendJsonResponse = (res, status, content) => {
     res.json(content);
 };
 
+var doAddReview = (req, res, location) => {
+    if (!location) {
+        sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+        });
+    } else {
+        location.reviews.push({
+            author: req.body.author,
+            rating: req.body.rating,
+            reviewText: req.body.reviewText
+        });
+        location.save((err, location) => {
+            var thisReview;
+            if (err) {
+                sendJsonResponse(res, 400, err);
+            } else {
+                updateAverageRating(location._id);
+                thisReview = location.reviews[location.reviews.length - 1];
+                sendJsonResponse(res, 201, thisReview);
+            }
+        });
+    }
+};
+
+var updateAverageRating = (locationid) => {
+    Loc
+        .findById(locationid)
+        .select('rating reviews')
+        .exec((err, location) => {
+            if (!err) {
+                doSetAverageRating(location);
+            }
+        });
+};
+
+var doSetAverageRating = (location) => {
+    var i, reviewCount, ratingAverage, ratingTotal;
+    if (location.reviews && location.reviews.length > 0) {
+        reviewCount = location.reviews.length;
+        ratingTotal = 0;
+        for (i = 0; i < reviewCount; i++) {
+            ratingTotal = ratingTotal + location.reviews[i].rating;
+        }
+        ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+        location.rating = ratingAverage;
+        location.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Average rating updated to", ratingAverage);
+            }
+        })
+    }
+};
+
 module.exports.reviewsCreate = (req, res) => {
-    sendJsonResponse(res, 200, { 'status': 'success' });
+    var locationid = req.params.locationid;
+    if (locationid) {
+        Loc
+            .findById(locationid)
+            .select('reviews')
+            .exec((err, location) => {
+                if (err) {
+                    sendJsonResponse(res, 400, err);
+                } else {
+                    doAddReview(req, res, location);
+                }
+            })
+    }
 };
 
 module.exports.reviewsReadOne = (req, res) => {
