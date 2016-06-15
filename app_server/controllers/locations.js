49ad8b7a0dc4621bddf03ce5bce9cfb21ebf2fd0
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-06-14 22:07:13 -0400

    getting ready to consume api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,13 @@
-module.exports.homelist = (req, res) => {
-    res.render('locations-list', { 
+var request = require('request');
+var apiOptions = {
+    server: "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://frozen-shelf-12883.herokuapp.com";
+}
+
+var renderHomepage = (req, res) => {
+    res.render('locations-list', {
         title: 'Loc8r | Home',
         pageHeader: {
             title: 'Loc8r',
@@ -11,23 +19,27 @@ module.exports.homelist = (req, res) => {
             rating: 3,
             facilities: ['Hot drinks', 'Food', 'Premium WiFi'],
             distance: '100m',
-        },{
-            name: 'Murphy\'s Pub',
-            address: 'Somewhere on Elm Street',
-            rating: 4,
-            facilities: ['Cold drinks', 'Good WiFi'],
-            distance: '127m',
-        },{
-            name: 'The Garrison Tavern',
-            address: 'Birmingham, England',
-            rating: 5,
-            facilities: ['Cold drinks', 'Danger', 'Premium WiFi'],
-            distance: '1250m',
-        }]
+        }, {
+                name: 'Murphy\'s Pub',
+                address: 'Somewhere on Elm Street',
+                rating: 4,
+                facilities: ['Cold drinks', 'Good WiFi'],
+                distance: '127m',
+            }, {
+                name: 'The Garrison Tavern',
+                address: 'Birmingham, England',
+                rating: 5,
+                facilities: ['Cold drinks', 'Danger', 'Premium WiFi'],
+                distance: '1250m',
+            }]
     });
 }
 
-module.exports.locationInfo = function(req, res) {
+module.exports.homelist = (req, res) => {
+    renderHomepage(req, res);
+}
+
+module.exports.locationInfo = function (req, res) {
     res.render('location-info', {
         title: 'Starcups',
         pageHeader: {
@@ -52,31 +64,31 @@ module.exports.locationInfo = function(req, res) {
                 closing: '7:00pm',
                 closed: false
             }, {
-                days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
-                closed: false
-            }, {
-                days: 'Sunday',
-                closed: true
-            }],
+                    days: 'Saturday',
+                    opening: '8:00am',
+                    closing: '5:00pm',
+                    closed: false
+                }, {
+                    days: 'Sunday',
+                    closed: true
+                }],
             reviews: [{
                 author: 'Simon Holmes',
                 rating: 5,
                 timestamp: '16 July 2013',
                 reviewText: 'What a great place. I can\'t say enough good things about it.'
             }, {
-                author: 'Charlie Chaplin',
-                rating: 3,
-                timestamp: '16 June 2013',
-                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-            }]
+                    author: 'Charlie Chaplin',
+                    rating: 3,
+                    timestamp: '16 June 2013',
+                    reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+                }]
         }
     });
 };
 
 /* GET 'Add review' page */
-module.exports.addReview = function(req, res) {
+module.exports.addReview = function (req, res) {
     res.render('location-review-form', {
         title: 'Review Starcups on Loc8r',
         pageHeader: {
