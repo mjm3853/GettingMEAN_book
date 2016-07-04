d4d5c10f3200371c822c5734d17eac38f86eded3
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-04 17:10:11 -0400

    validating at the application level

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -169,17 +169,22 @@ module.exports.doAddReview = function (req, res) {
         method: "POST",
         json: postdata
     };
-    request(
-        requestOptions,
-        function (err, response, body) {
-            if (response.statusCode === 201) {
-                res.redirect('/location/' + locationid);
-            } else if (response.statusCode === 400 && body.name && body.name === "ValidationError") {
-                res.redirect('/location/' + locationid + '/reviews/new?err=val');
-            } else {
-                console.log(body);
-                _showError(req, res, response.statusCode);
+    if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+        res.redirect('/location/' + locationid + '/reviews/new?err=val');
+    } else {
+        request(
+            requestOptions,
+            function (err, response, body) {
+                if (response.statusCode === 201) {
+                    res.redirect('/location/' + locationid);
+                } else if (response.statusCode === 400 && body.name && body.name === "ValidationError") {
+                    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+                } else {
+                    console.log(body);
+                    _showError(req, res, response.statusCode);
+                }
             }
-        }
-    )
+        )
+    }
+
 };
