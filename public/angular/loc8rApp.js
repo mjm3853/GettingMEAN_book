e3104897822acb7fd2d333a064e8683f4e006193
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-05 21:29:16 -0400

    add format distance filter

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,5 +1,27 @@
 angular.module('loc8rApp', []);
 
+var _isNumeric = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var formatDistance = function() {
+    return function (distance) {
+        var numDistance, unit;
+        if (distance && _isNumeric(distance)) {
+            if (distance > 1) {
+                numDistance = parseFloat(distance).toFixed(1);
+                unit = 'km';
+            } else {
+                numDistance = parseInt(distance * 1000,10);
+                unit = 'm';
+            }
+            return numDistance + unit;
+        } else {
+            return "?";
+        }
+    };
+};
+
 var locationListCtrl = function ($scope) {
     $scope.data = {
         locations: [{
@@ -22,4 +44,5 @@ var locationListCtrl = function ($scope) {
 
 angular
   .module('loc8rApp')
-  .controller('locationListCtrl', locationListCtrl);
+  .controller('locationListCtrl', locationListCtrl)
+  .filter('formatDistance', formatDistance);
