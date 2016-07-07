f81223556f35cae333da77801724c0d08bf84931
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-07 19:39:30 -0400

    async angular request for location list data

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -4,7 +4,7 @@ var _isNumeric = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var formatDistance = function() {
+var formatDistance = function () {
     return function (distance) {
         var numDistance, unit;
         if (distance && _isNumeric(distance)) {
@@ -12,7 +12,7 @@ var formatDistance = function() {
                 numDistance = parseFloat(distance).toFixed(1);
                 unit = 'km';
             } else {
-                numDistance = parseInt(distance * 1000,10);
+                numDistance = parseInt(distance * 1000, 10);
                 unit = 'm';
             }
             return numDistance + unit;
@@ -24,35 +24,30 @@ var formatDistance = function() {
 
 var ratingStars = function () {
     return {
-        scope : {
-          thisRating : '=rating'  
+        scope: {
+            thisRating: '=rating'
         },
-        templateUrl : '/angular/rating-stars.html'
+        templateUrl: '/angular/rating-stars.html'
     }
 }
 
-var locationListCtrl = function ($scope) {
-    $scope.data = {
-        locations: [{
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '0.296456',
-            _id: '5370a35f2536f6785f8dfb6a'
-        }, {
-                name: 'Costy',
-                address: '125 High Street, Reading, RG6 1PS',
-                rating: 5,
-                facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
-                distance: '0.7865456',
-                _id: '5370a35f2536f6785f8dfb6a'
-            }]
-    };
+var loc8rData = function ($http) {
+    return $http.get('/api/locations?lng=72.9690884&lat=20.45504&maxDistance=2000')
+};
+
+var locationListCtrl = function ($scope, loc8rData) {
+    loc8rData
+        .success(function (data) {
+            $scope.data = { locations: data };
+        })
+        .error(function (e) {
+            console.log(e);
+        });
 };
 
 angular
-  .module('loc8rApp')
-  .controller('locationListCtrl', locationListCtrl)
-  .filter('formatDistance', formatDistance)
-  .directive('ratingStars', ratingStars);
+    .module('loc8rApp')
+    .controller('locationListCtrl', locationListCtrl)
+    .filter('formatDistance', formatDistance)
+    .directive('ratingStars', ratingStars)
+    .service('loc8rData', loc8rData);
