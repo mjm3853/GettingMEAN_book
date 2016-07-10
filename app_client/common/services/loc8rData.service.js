1bb5a5c5bbea9c0d40c8de77192c4b8bb383c3ab
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-10 10:12:48 -0400

    broken. not seeing viewModel after adding services

---
+++ app_client/common/services/loc8rData.service.js
@@ -0,0 +1,12 @@
+angular
+    .module('loc8rApp')
+    .service('loc8rData', loc8rData);
+
+var loc8rData = function ($http) {
+    var locationByCoords = function (lat, lng) {
+        return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=2000000');
+    };
+    return {
+        locationByCoords: locationByCoords
+    }
+};
