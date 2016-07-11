31e597ba19685ef15aeb33518ba78c535ebb9177
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-10 22:38:52 -0400

    ch10 magic

--- app_client/common/directives/footerGeneric/footerGeneric.directive.js
+++ app_client/common/directives/footerGeneric/footerGeneric.directive.js
@@ -1,13 +1,14 @@
-(function (){
-    
-    angular
-        .module('loc8rApp')
-        .directive('footerGeneric', footerGeneric);
-        
-    function footerGeneric () {
-        return {
-            restrict: 'EA',
-            templateUrl: '/common/directives/footerGeneric/footerGeneric.template.html'
-        };
-    }
+(function () {
+
+  angular
+    .module('loc8rApp')
+    .directive('footerGeneric', footerGeneric);
+
+  function footerGeneric () {
+    return {
+      restrict: 'EA',
+      templateUrl: '/common/directives/footerGeneric/footerGeneric.template.html'
+    };
+  }
+
 })();
--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -1,16 +1,18 @@
 (function () {
-    angular
-        .module('loc8rApp')
-        .directive('ratingStars', ratingStars);
 
-    function ratingStars() {
-        return {
-            restrict: 'EA',
-            scope: {
-                thisRating: '=rating'
-            },
-            templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
-        }
-    }
+  angular
+    .module('loc8rApp')
+    .directive('ratingStars', ratingStars);
+
+  function ratingStars () {
+    return {
+      restrict: 'EA',
+      scope: {
+        thisRating : '=rating'
+      },
+      templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+    };
+  }
+
+
 })();
-
