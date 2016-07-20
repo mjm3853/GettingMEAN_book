bfd16d9831798932113f5887a8b385e59e40c96f
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-19 21:56:04 -0400

    working login logout on navigation

--- app_client/app.js
+++ app_client/app.js
@@ -2,7 +2,7 @@
 
   angular.module('loc8rApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
 
-  function config ($routeProvider, $locationProvider) {
+  function config($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: '/home/home.view.html',
@@ -24,7 +24,12 @@
         controller: 'registerCtrl',
         controllerAs: 'vm'
       })
-      .otherwise({redirectTo: '/'});
+      .when('/login', {
+        templateUrl: '/auth/login/login.view.html',
+        controller: 'loginCtrl',
+        controllerAs: 'vm'
+      })
+      .otherwise({ redirectTo: '/' });
 
     // use the HTML5 History API
     $locationProvider.html5Mode(true);
