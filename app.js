10f37409b27bdc976ea6db00f89af9ff28db4326
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-07-19 21:44:21 -0400

    allows customer to register. token in local storage

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ require('./app_api/config/passport');
 
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
-// var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -23,28 +22,30 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
 var appClientFiles = [
-    'app_client/app.js',
-    'app_client/home/home.controller.js',
-    'app_client/about/about.controller.js',
-    'app_client/locationDetail/locationDetail.controller.js',
-    'app_client/reviewModal/reviewModal.controller.js',
-    'app_client/common/services/geolocation.service.js',
-    'app_client/common/services/loc8rData.service.js',
-    'app_client/common/filters/formatDistance.filter.js',
-    'app_client/common/filters/addHtmlLineBreaks.filter.js',
-    'app_client/common/directives/navigation/navigation.directive.js',
-    'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
-    'app_client/common/directives/pageHeader/pageHeader.directive.js',
-    'app_client/common/directives/ratingStars/ratingStars.directive.js'
+  'app_client/app.js',
+  'app_client/home/home.controller.js',
+  'app_client/about/about.controller.js',
+  'app_client/auth/register/register.controller.js',
+  'app_client/locationDetail/locationDetail.controller.js',
+  'app_client/reviewModal/reviewModal.controller.js',
+  'app_client/common/services/authentication.service.js',
+  'app_client/common/services/geolocation.service.js',
+  'app_client/common/services/loc8rData.service.js',
+  'app_client/common/filters/formatDistance.filter.js',
+  'app_client/common/filters/addHtmlLineBreaks.filter.js',
+  'app_client/common/directives/navigation/navigation.directive.js',
+  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+  'app_client/common/directives/pageHeader/pageHeader.directive.js',
+  'app_client/common/directives/ratingStars/ratingStars.directive.js'
 ];
-var uglified = uglifyJs.minify(appClientFiles, { compress: false });
+var uglified = uglifyJs.minify(appClientFiles, { compress : false });
 
-fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Script generated and saved:", 'loc8r.min.js');
-    }
+fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Script generated and saved:", 'loc8r.min.js');
+  }
 });
 
 // uncomment after placing your favicon in /public
@@ -56,37 +57,33 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use(passport.initialize());
-
 // app.use('/', routes);
 app.use('/api', routesApi);
-// app.use('/users', users);
 
-app.use(function (req, res) {
-    res.sendfile(path.join(_dirname, 'app_client', 'index.html'));
-})
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handlers
-
-//Catch unauthorized errors
+// Catch unauthorised errors
 app.use(function (err, req, res, next) {
-    if (err.name === "UnauthorizedError") {
-        res.status(401);
-        res.json({ "message": err.name + ": " + err.message })
-    }
-})
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
+});
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -97,7 +94,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
